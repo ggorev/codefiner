108756bf161a838642f176ddc67815c29934765e
George <George@211.ru>
2023-11-17 17:33:28 +0700

    added alembic and model predictions

---
+++ src/api/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Float
+
+from src.data.database import Base
+
+
+class Predictions(Base):
+    __tablename__ = "predictions"
+
+    request_id = Column(Integer, primary_key=True)
+    type = Column(String, nullable=False)
+    path_or_link = Column(String, nullable=False)
+    file_extension = Column(String, nullable=False)
+    prediction = Column(String, nullable=False)
+    python_probability = Column(Float, nullable=False)
+    java_probability = Column(Float, nullable=False)
+    javascript_probability = Column(Float, nullable=False)
--- src/data/database.py
+++ src/data/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-from src.config import settings
+from src.data.config import settings
 
 engine = create_async_engine(settings.DB_URL)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
